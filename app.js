d8ffbf584a19a4f3a4e87c3f1e4e615d831be759
RoryLetteney <rory.letteney@gmail.com>
2019-03-18 18:03:43 -0500

    Rendered store data to sales.html

--- app.js
+++ app.js
@@ -5,9 +5,31 @@ var randomNumberOfCustomers = (min, max) => {
   return Math.ceil(Math.random() * (max - (min - 1)) + (min - 1));
 };
 
+var generateLists = (storeData) => {
+  var sales = document.getElementById('sales');
+  var h2 = document.createElement('h2');
+  var ul = document.createElement('ul');
+  var total = document.createElement('li');
+  h2.className = 'store-name';
+  h2.textContent = storeData.locationName;
+  ul.className = 'store-data';
+  total.className = 'store-data-item';
+  total.textContent = `Total: ${storeData.totalDailyCookies} cookies`;
+
+  for (var i = 0; i < storeClose - storeOpen; i++) {
+    var li = document.createElement('li');
+    li.className = 'store-data-item';
+    li.textContent = `${(i + storeOpen) < 13 ? i + storeOpen : i - storeOpen}${(i + storeOpen) < 13 ? 'am' : 'pm'}: ${storeData.results[1][i]} cookies`;
+    ul.appendChild(li);
+  }
+  sales.appendChild(h2);
+  sales.appendChild(ul);
+  ul.appendChild(total);
+};
+
 var stores = [
   {
-    locationName: 'First and Pike',
+    locationName: '1st and Pike',
     minCustomersPerHour: 23,
     maxCustomersPerHour: 65,
     avgCookiesPerCustomer: 6.3,
@@ -41,7 +63,11 @@ var stores = [
       this.calcCustomersPerHour();
       this.calcCookiesPerHour();
       this.calcTotalDailyCookies();
-      this.results.push(this.customersPerHour, this.cookiesPerHour, this.totalDailyCookies);
+      this.results.push(
+        this.customersPerHour,
+        this.cookiesPerHour,
+        this.totalDailyCookies
+      );
     }
   },
   {
@@ -200,4 +226,5 @@ var stores = [
 
 stores.forEach(store => {
   store.render();
+  generateLists(store);
 });
