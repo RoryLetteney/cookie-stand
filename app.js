bfe985edd70dfe3b28340dc2f70285a7a0a89e29
RoryLetteney <rory.letteney@gmail.com>
2019-03-18 17:02:05 -0500

    Created remaining store location objects.

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ var randomNumberOfCustomers = (min, max) => {
   return Math.ceil(Math.random() * (max - (min - 1)) + (min - 1));
 };
 
-var stores = {
-  firstAndPike: {
+var stores = [
+  {
     locationName: 'First and Pike',
     minCustomersPerHour: 23,
     maxCustomersPerHour: 65,
@@ -43,5 +43,161 @@ var stores = {
       this.calcTotalDailyCookies();
       this.results.push(this.customersPerHour, this.cookiesPerHour, this.totalDailyCookies);
     }
+  },
+  {
+    locationName: 'SeaTac Airport',
+    minCustomersPerHour: 3,
+    maxCustomersPerHour: 24,
+    avgCookiesPerCustomer: 1.2,
+    customersPerHour: [],
+    cookiesPerHour: [],
+    totalDailyCookies: 0,
+    results: [],
+    calcCustomersPerHour: function() {
+      for (var i = 0; i < (storeClose - storeOpen); i++) {
+        this.customersPerHour.push(
+          randomNumberOfCustomers(
+            this.minCustomersPerHour,
+            this.maxCustomersPerHour
+          )
+        );
+      }
+    },
+    calcCookiesPerHour: function() {
+      for (var i = 0; i < this.customersPerHour.length; i++) {
+        this.cookiesPerHour.push(
+          Math.round(this.customersPerHour[i] * this.avgCookiesPerCustomer)
+        );
+      }
+    },
+    calcTotalDailyCookies: function() {
+      this.cookiesPerHour.forEach(i => {
+        this.totalDailyCookies += i;
+      });
+    },
+    render: function() {
+      this.calcCustomersPerHour();
+      this.calcCookiesPerHour();
+      this.calcTotalDailyCookies();
+      this.results.push(this.customersPerHour, this.cookiesPerHour, this.totalDailyCookies);
+    }
+  },
+  {
+    locationName: 'Seattle Center',
+    minCustomersPerHour: 11,
+    maxCustomersPerHour: 38,
+    avgCookiesPerCustomer: 3.7,
+    customersPerHour: [],
+    cookiesPerHour: [],
+    totalDailyCookies: 0,
+    results: [],
+    calcCustomersPerHour: function() {
+      for (var i = 0; i < (storeClose - storeOpen); i++) {
+        this.customersPerHour.push(
+          randomNumberOfCustomers(
+            this.minCustomersPerHour,
+            this.maxCustomersPerHour
+          )
+        );
+      }
+    },
+    calcCookiesPerHour: function() {
+      for (var i = 0; i < this.customersPerHour.length; i++) {
+        this.cookiesPerHour.push(
+          Math.round(this.customersPerHour[i] * this.avgCookiesPerCustomer)
+        );
+      }
+    },
+    calcTotalDailyCookies: function() {
+      this.cookiesPerHour.forEach(i => {
+        this.totalDailyCookies += i;
+      });
+    },
+    render: function() {
+      this.calcCustomersPerHour();
+      this.calcCookiesPerHour();
+      this.calcTotalDailyCookies();
+      this.results.push(this.customersPerHour, this.cookiesPerHour, this.totalDailyCookies);
+    }
+  },
+  {
+    locationName: 'Capitol Hill',
+    minCustomersPerHour: 20,
+    maxCustomersPerHour: 38,
+    avgCookiesPerCustomer: 2.3,
+    customersPerHour: [],
+    cookiesPerHour: [],
+    totalDailyCookies: 0,
+    results: [],
+    calcCustomersPerHour: function() {
+      for (var i = 0; i < (storeClose - storeOpen); i++) {
+        this.customersPerHour.push(
+          randomNumberOfCustomers(
+            this.minCustomersPerHour,
+            this.maxCustomersPerHour
+          )
+        );
+      }
+    },
+    calcCookiesPerHour: function() {
+      for (var i = 0; i < this.customersPerHour.length; i++) {
+        this.cookiesPerHour.push(
+          Math.round(this.customersPerHour[i] * this.avgCookiesPerCustomer)
+        );
+      }
+    },
+    calcTotalDailyCookies: function() {
+      this.cookiesPerHour.forEach(i => {
+        this.totalDailyCookies += i;
+      });
+    },
+    render: function() {
+      this.calcCustomersPerHour();
+      this.calcCookiesPerHour();
+      this.calcTotalDailyCookies();
+      this.results.push(this.customersPerHour, this.cookiesPerHour, this.totalDailyCookies);
+    }
+  },
+  {
+    locationName: 'Alki',
+    minCustomersPerHour: 2,
+    maxCustomersPerHour: 16,
+    avgCookiesPerCustomer: 4.6,
+    customersPerHour: [],
+    cookiesPerHour: [],
+    totalDailyCookies: 0,
+    results: [],
+    calcCustomersPerHour: function() {
+      for (var i = 0; i < (storeClose - storeOpen); i++) {
+        this.customersPerHour.push(
+          randomNumberOfCustomers(
+            this.minCustomersPerHour,
+            this.maxCustomersPerHour
+          )
+        );
+      }
+    },
+    calcCookiesPerHour: function() {
+      for (var i = 0; i < this.customersPerHour.length; i++) {
+        this.cookiesPerHour.push(
+          Math.round(this.customersPerHour[i] * this.avgCookiesPerCustomer)
+        );
+      }
+    },
+    calcTotalDailyCookies: function() {
+      this.cookiesPerHour.forEach(i => {
+        this.totalDailyCookies += i;
+      });
+    },
+    render: function() {
+      this.calcCustomersPerHour();
+      this.calcCookiesPerHour();
+      this.calcTotalDailyCookies();
+      this.results.push(this.customersPerHour, this.cookiesPerHour, this.totalDailyCookies);
+    }
   }
-};
+];
+
+stores.forEach(store => {
+  store.render();
+});
