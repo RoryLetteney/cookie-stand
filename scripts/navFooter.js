972eb6902363ebeacc76d374c1271fa09f3aa27c
RoryLetteney <rory.letteney@gmail.com>
2019-03-21 12:51:41 -0500

    Added new images to imgs folder. Moved footer to js file to generate it on all subsequent pages.

---
+++ scripts/navFooter.js
@@ -0,0 +1,5 @@
+var body = document.body;
+var footer = document.createElement('footer');
+footer.classList.add('footer');
+footer.innerHTML = '2019 &copy; Pat\'s Salmon Cookies';
+body.appendChild(footer);
